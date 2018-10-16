de6c4aa826b01af95a389bf7b0de417393987d7b
caique <caique.1810@hotmail.com>
2018-10-16 14:32:09 -0300

    teste 16/10

---
+++ teste.py
@@ -0,0 +1,24 @@
+#Comentarios
+
+b = 1
+s = 0
+a = [int(input("digite o primeiro número da lista"))]
+while (b !=0):
+ b = int(input("digite mais um número da lista digite para sair"))
+ a.append(b)
+ s= s+1
+
+del(a[s])
+
+a.sort()
+t=len(a) - 2
+
+s = 0
+while (s < t):
+    if (a[s] == a[s+1]):
+        del(a[s])
+        t=t-1
+        s=s-1
+    s=s+1
+ 
+print(a)
